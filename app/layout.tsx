590a34fea2e3287ffcc79da33a72adf8bd96b224
Admin <kamleshksks456@gmail.com>
2025-10-10 16:02:10 +0000

    fix: remove unused GoogleLogin import from layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { GoogleLogin, GoogleOAuthProvider } from "@react-oauth/google";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
